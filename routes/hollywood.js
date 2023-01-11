b64654ec5b5324c040ec3dfbd71164d8e9fc6bf0
MeenakshiPillai20 <109613222+MeenakshiPillai20@users.noreply.github.com>
2023-01-11 11:12:15 +0530

    Delete hollywood.js

--- routes/hollywood.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const hollywoodData = require('../controllers/hollywood');
-const mainRouter = express.Router();
-// mainRouter.get('/hollywood', APIdata.getData)
-mainRouter.route("/hollywoodPost").get(hollywoodData.getHollywoodPost)
-mainRouter.route("/hollywoodFirstPost").get(hollywoodData.getHollywoodFirstPost)
-mainRouter.route("/hollywoodTopPost").get(hollywoodData.getHollywoodTopPost)
-module.exports = mainRouter
