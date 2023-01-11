b0199ca947e0f5608739e1735ea014876a3d52ba
MeenakshiPillai20 <109613222+MeenakshiPillai20@users.noreply.github.com>
2023-01-11 11:13:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const hollywood = require('./routes/hollywood');
+
 const bollywood = require('./routes/bollywood');
 const cors = require('cors')
 
 app.use(cors());
 
-app.use("/",hollywood);
+
 app.use("/",bollywood);
 
 app.listen(9000,function(){
